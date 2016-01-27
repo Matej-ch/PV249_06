aaf257fd56416585b7e7f7cac76f251f58d24dad
Matej-ch <matejchalachan@gmail.com>
2016-01-27 17:25:33 +0100

    init

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,36 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  before_filter do
+    resource = controller_name.singularize.to_sym
+    method = "#{resource}_params"
+    params[resource] &&= send(method) if respond_to?(method, true)
+  end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email,:username,
+                                                            :password,
+                                                            :password_confirmation,
+                                                            :remember_me) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email,:username,
+                                                            :password,
+                                                            :password_confirmation,
+                                                            :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username,
+                                                                   :email,
+                                                                   :current_password,
+                                                                   :password,
+                                                                   :password_confirmation,
+                                                                   :avatar) }
+  end
+
+end
